d869ce7ac17fcd599c6dfeaf49abe52271482686
Kerbin28 <zhank15@mcmaster.ca>
2021-09-26 23:15:12 -0400

    Update main.py
    Added more documentation, redesigned content layout format, added a register username button.
    **NOTE: This program will create a runtime error when exited, will need to resolve in the future

--- DCM Code/main.py
+++ DCM Code/main.py
@@ -16,7 +16,8 @@ class Run:
 
     def __init__(self):
         # The login window object is created
-        LoginWindow()
+        ContentWindow()
+        ContentWindow.__root.mainloop()
 
 
 """
@@ -25,7 +26,7 @@ class Run:
 """
 
 
-class LoginWindow(tk.Tk):
+class LoginWindow(tk.Frame):
     # Variable Declaration
     # Constants
     WIDTH = 30
@@ -33,16 +34,22 @@ class LoginWindow(tk.Tk):
     FONT = ("Arial", 12)
     DEFAULT_USERNAME_TEXT = "Username"
     DEFAULT_PASSWORD_TEXT = "Password"
-    DEFAULT_BUTTON_TEXT = "Submit"
+    DEFAULT_SUBMIT_BUTTON_TEXT = "Submit"
+    DEFAULT_REGISTER_BUTTON_TEXT = "Register"
     PADDING = 10
+    BACKGROUND_COLOR = "#0059b3"
+
     # Private Variables
-    __root = Tk()
+    __root = None
+    __mainWindow = None
     __usernameField = None
     __passwordField = None
     __submitButton = None
+    __registerButton = None
     __password = "Password"
     __username = "Username"
     __contentPane = None
+    __buttonFrame = None
     # Public Variables
 
     """
@@ -50,20 +57,24 @@ class LoginWindow(tk.Tk):
         @param self
     """
 
-    def __init__(self):
+    def __init__(self, mainWindow):
+        self.__mainWindow = mainWindow
+        self.__root = mainWindow.getRoot()
         # Initialize components of frame
         self.__initializeEntryFields()
         self.__initializeButtons()
 
-        self.__contentPane = PanedWindow(self.__root)
+        self.__contentPane = Frame(self.__root, bg="red")
         self.__contentPane.pack(anchor=tkinter.CENTER)
-        self.__contentPane.config(bg="red")
         # Initialize frame properties
-        print()
         self.__root.title("DCM")
         self.__root.geometry("500x450")
-        self.__root.config(bg='#0059b3')
-        self.__root.mainloop()
+        self.__root.config(bg=self.BACKGROUND_COLOR)
+
+    """
+        Initializes the text fields and adds formatting to them
+        @param self
+    """
 
     def __initializeEntryFields(self):
         self.__usernameField = Entry(self.__contentPane, width=self.WIDTH, font=self.FONT)
@@ -74,15 +85,33 @@ class LoginWindow(tk.Tk):
         self.__passwordField.insert(tk.END, self.DEFAULT_PASSWORD_TEXT)
         self.__passwordField.pack(pady=self.PADDING)
 
+    """
+       Initializes the buttons and adds formatting to them
+       @param self
+    """
+
     def __initializeButtons(self):
-        self.__submitButton = Button(self.__contentPane, text=self.DEFAULT_BUTTON_TEXT, command=self.getText,
+        self.__buttonFrame = Frame(self.__contentPane, bg=self.BACKGROUND_COLOR)
+        self.__submitButton = Button(self.__buttonFrame, text=self.DEFAULT_SUBMIT_BUTTON_TEXT, command=self.getText,
                                      relief="flat")
-        self.__submitButton.pack()
+        self.__submitButton.grid(row=0, column=0, padx=5, pady=10)
+
+        self.__registerButton = Button(self.__buttonFrame, text=self.DEFAULT_REGISTER_BUTTON_TEXT,
+                                       command=self.registerUser,
+                                       relief="flat")
+        self.__registerButton.grid(row=0, column=1, padx=5, pady=10)
+
+        self.__buttonFrame.pack()
+
+    """
+        Function is called when the submit button is pressed, validates the password to the stored file and 
+        will enter the DCM window if successful
+        @param self
+    """
 
     def getText(self):
         self.__password = self.__passwordField.get()
         self.__username = self.__usernameField.get()
-
         # DEBUGGING: Test output, remove when no longer needed
         print(self.__username, " Username")
         print(self.__password, " Password")
@@ -92,13 +121,63 @@ class LoginWindow(tk.Tk):
         # --Note: figure out a way to only remove content pane instead of removing all elements in content pane
         """
         if(1):
-            self.__contentPane.pack_forget()
-            self.__submitButton.pack_forget()
-            self.__passwordField.pack_forget()
-            self.__usernameField.pack_forget()
             
         """
 
+    """
+        Function opens a frame to add new credentials if applicable
+        @param self
+    """
 
+    def registerUser(self):
+        pass
+
+    """
+    Getter functions
+    """
+
+    def getRoot(self):
+        return self.__root
+
+
+"""
+    Frame to enter a new password and username to the storage file
+"""
+
+
+class RegisterWindow(tk.Frame):
+    pass
+
+
+"""
+The ContentWindow stores all the visual components of the DCM
+@param tk.Tk
+"""
+
+
+class ContentWindow(tk.Tk):
+    # Variables
+    __root = Tk()
+    loginWindow = None
+    """
+         Object Constructor
+         @param self
+    """
+
+    def __init__(self):
+        self.__root.title("DCM")
+        self.__root.geometry("500x450")
+        self.__root.config(bg='#0059b3')
+        loginWindow = LoginWindow(self)
+        loginWindow.getRoot().mainloop()
+        self.__root.pack()
+
+    def getRoot(self):
+        return self.__root
+
+    def login(self):
+        pass
+
+# Main script
 if __name__ == "__main__":
     run = Run()
